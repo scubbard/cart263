414bb65b18dd7dd743ae6a28964ab21de10acc4c
scubbard <geomcf@gmail.com>
2022-02-28 19:21:51 -0500

    lighthouse: added comments

--- sailor generator/js/script.js
+++ sailor generator/js/script.js
@@ -33,7 +33,7 @@ let secretAnswer = [`Where'd ye learn that name, boy?`, `That's not a name ye sh
 let answerUsed;
 
 /**
-Description of preload
+loads all json files and assets
 */
 function preload() {
   proverbData = loadJSON(`assets/data/sailor.json`);
@@ -52,7 +52,7 @@ function preload() {
 
 
 /**
-Description of setup
+creates canvas, sets delay between animation frames, and initializes animations
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -69,15 +69,14 @@ function setup() {
   talkSprite = createSprite(width / 2, height / 2);
   talkSprite.addAnimation("talk", talkAnim);
 }
-
+//creates `title screen` and activates animates
 function start() {
   background(30, 40, 60);
   drawSprite(startSprite);
 }
-
+//function called when the state is `game`
 function game() {
   background(60, 20, 20);
-  //wordTyped();
   keyPressed();
   proverbText();
   drawSprite(roomSprite);
@@ -93,23 +92,20 @@ function game() {
 }
 
 /**
-Description of draw()
+consistently running function to check which state the game is/if anything is being said
 */
 function draw() {
   stateCheck();
   talkCheck();
-  //print(state)
 }
 
-
-function animate() {}
-
+//the `click to start` function
 function mousePressed() {
   if (state === `start`) {
     state = `game`;
   }
 }
-
+//loads a random proverb from a preloaded list and calls responsiveVoice to say it aloud
 function proverbSpeak() {
   proverb = random(proverbData.proverbs);
   if (!responsiveVoice.isPlaying()) {
@@ -120,6 +116,7 @@ function proverbSpeak() {
   }
 }
 
+//loads one of the `secret answers` and calls responsiveVoice to say it aloud
 function secretSpeak() {
     let answerUsed = random(secretAnswer);
   if (!responsiveVoice.isPlaying()) {
@@ -129,13 +126,13 @@ function secretSpeak() {
     })
   }
 }
-
+//logs keys being typed and adds them to the `typing` string
 function keyTyped() {
   stroke(0);
   typing += key;
 }
 
-
+//checks if the word being typed should trigger the proverb answer or a secret answer
 function wordCheck() {
   currentWord = typing
   if (!secretWords.includes(currentWord)) {
@@ -144,7 +141,7 @@ function wordCheck() {
     state = `secretTalking`
   }
 }
-
+//checks if a key is pressed that will delete the phrase or enter it
 function keyPressed() {
   if (keyCode === BACKSPACE) {
     typing = typing.substring(0, typing.length - 1);
@@ -154,32 +151,33 @@ function keyPressed() {
     textDelete();
   }
 }
-
+//function that deletes all text in the `typing` string
 function textDelete() {
   typing = ``;
 }
-
+//state function called when responsiveVoice is speaking
 function talking() {
   background(60, 20, 20);
   drawSprite(talkSprite);
   proverbSpeak();
-
 }
-
+//state function called when responsive voice is telling a secret answer
 function secretTalking() {
+  secretText();
   background(60, 20, 20);
   drawSprite(talkSprite);
   secretSpeak();
 
 }
 
+//function that prevents talking state from continuously being called
 function talkCheck(){
   if (!responsiveVoice.isPlaying() && state != `start`){
     state = `game`
   }
 }
 
-
+//displays proverb text
 function proverbText() {
   push();
   textSize(23);
@@ -190,7 +188,18 @@ function proverbText() {
   text(`"${proverb}"`, width / 2, height - height / 6, width / 2);
   pop();
 }
-
+//displays secret text
+function secretText() {
+  push();
+  textSize(23);
+  textFont(myFont);
+  textAlign(CENTER);
+  fill(0);
+  rectMode(CENTER);
+  text(`"${answerUsed}"`, width / 2, height - height / 6, width / 2);
+  pop();
+}
+//checks states and calls correlating function
 function stateCheck() {
   if (state === `start`) {
     start();
