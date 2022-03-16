93b70c298edd08c34f517a8be1ba86e29bca0096
scubbard <geomcf@gmail.com>
2022-03-16 16:58:01 -0400

    started get code activity

---
+++ activities/jqueryUI/js/script.js
@@ -0,0 +1,54 @@
+/**
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+// Code goes here
+
+let secret = `Theremin`;
+
+$(`#solved-dialog`).dialog({
+  autoOpen: false,
+
+  buttons: {
+    "yeah!": function(){
+      $(this).dialog(`close`);
+    }
+  }
+});
+
+//highlights secret letters on mouseover
+$(`.secret`).on(`mouseover`, function(event){
+  $(this).addClass(`found`, 500);
+});
+
+//makes secret letters draggable
+$(`.secret`).draggable({
+  helper:`clone`
+});
+
+//makes answer area droppable
+$(`#answer`).droppable({
+  drop: function(event,ui) {
+    //get the letter in the dragged element
+    let letter = ui.draggable.text();
+    //adds letter to answer box
+    $(this).append(letter);
+    //disables dragging
+    ui.draggable.draggable(`disable`);
+
+    ui.draggable.removeClass(`found`,500);
+
+    ui.draggable.off(`mouseover`);
+
+    if ($(`#answer`).text() === secret) {
+      $(`#solved-dialog`).dialog(`open`);
+    }
+
+  }
+});
